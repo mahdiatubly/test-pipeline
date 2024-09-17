6f8b4b740f0786a54f73b357b20456ab28c1f27b
MA <66721110+mahdiatubly@users.noreply.github.com>
2024-09-17 15:11:01 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "You have to love Jenkins, he is the best of thje best"
+echo "You have to love Jenkins, he is the best of the best"
 
 # Keep the container alive
 tail -f /dev/null
